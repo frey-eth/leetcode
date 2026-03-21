7e5c10d99075889e4c9a1a3c37783f76983aa58f
Frey <83070538+frey-eth@users.noreply.github.com>
2026-03-21 14:50:38 +0700

    Time: 2 ms (54.55%), Space: 65.5 MB (45.45%) - LeetHub

---
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.ts
@@ -0,0 +1,11 @@
+function reverseSubmatrix(grid, x, y, k) {
+    for (let i = 0; i < Math.floor(k/2); i++) {
+        for(let j=y; j < y+k; j++) {
+            const flag = grid[x+i][j]
+            grid[x+i][j] = grid[x+k-i-1][j]
+            grid[x+k-i-1][j] = flag
+        }
+    }
+
+    return grid
+};
